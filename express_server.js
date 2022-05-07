194277ac990e5bc84866ec6a9fdc095235272da7
Aidan Antony <aidanantony65@gmail.com>
2022-05-07 13:32:13 -0600

    Add login featurea along with logout feature and cookies

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 function generateRandomString() {
   let input = 6;
@@ -22,7 +24,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username:req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -35,7 +37,8 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVar = {username: req.cookies["username"]}
+  res.render("urls_new", templateVar);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -74,8 +77,9 @@ app.post("/urls/:shortURL/delete", (req,res) => {
 
 app.get("/urls/:id/edit", (req,res) => {
   const shortUrl = req.params.id
-  const templateVar = {shortURL: shortUrl, longURL: urlDatabase[shortUrl]}
+  const templateVar = {shortURL: shortUrl, longURL: urlDatabase[shortUrl], username: req.cookies.username}
   console.log(shortUrl)
+  console.log("templateVar:", templateVar)
   res.render("urls_show", templateVar)
 })
 
@@ -86,5 +90,15 @@ app.post("/urls/:id", (req,res) => {
   console.log('label', userInput)
   console.log(shortUrl)
   console.log(urlDatabase)
+  res.redirect("/urls")
+})
+
+app.post("/login", (req,res) => {
+  res.cookie("username", req.body['username'])
+  res.redirect("/urls")
+})
+
+app.post("/logout", (req,res) => {
+  res.clearCookie("username")
   res.redirect("/urls")
 })
