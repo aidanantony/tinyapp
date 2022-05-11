f1a9df76519f7d1ddecb9c60a72d18880268af0e
Aidan Antony <aidanantony65@gmail.com>
2022-05-11 17:28:54 -0600

    The test file with mocha and chai to test the helper functions

---
+++ test/helperTests.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helper.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user, testUsers['userRandomID'])
+  });
+  it("should return undefined if an email is non existent", function() {
+  let user = getUserByEmail("test@testing.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user, undefined)
+});
+});
