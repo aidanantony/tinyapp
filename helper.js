1edfac3b05170f957caeb5eb0181c8ffc65b8bd0
Aidan Antony <aidanantony65@gmail.com>
2022-05-11 17:31:15 -0600

    Export of the helper functions from this file

--- helper.js
+++ helper.js
@@ -1,10 +1,11 @@
-const emailLookup = function (email) {
-  for (let user in users) {
-    if (users[user].email === email) {
-      return users[user];
+const getUserByEmail = function (email, database) {
+  for (let user in database) {
+    if (database[user].email === email) {
+      return database[user];
     }
   }
   return undefined;
 }
 
-module.exports = emailLookup
+
+module.exports = { getUserByEmail }
