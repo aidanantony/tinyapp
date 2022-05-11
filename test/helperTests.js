af76ae6812d8893941d96da5b371f77aec084df0
Aidan Antony <aidanantony65@gmail.com>
2022-05-11 17:51:06 -0600

    Lint fixes and clean up

--- test/helperTests.js
+++ test/helperTests.js
@@ -4,26 +4,24 @@ const { getUserByEmail } = require('../helper.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
-    const expectedUserID = "userRandomID";
-    assert.equal(user, testUsers['userRandomID'])
+    const user = getUserByEmail("user@example.com", testUsers);
+    assert.equal(user, testUsers['userRandomID']);
   });
   it("should return undefined if an email is non existent", function() {
-  let user = getUserByEmail("test@testing.com", testUsers)
-    const expectedUserID = "userRandomID";
-    assert.equal(user, undefined)
-});
+    let user = getUserByEmail("test@testing.com", testUsers);
+    assert.equal(user, undefined);
+  });
 });
